049eceec028c2f9184ea46fa8d7933ca0567a0cf
Isa <isaismzade@gmail.com>
2024-05-30 16:58:04 +0400

    updated fourth time

--- src/com/javarush/task/pro/task15/task1504/Solution.java
+++ src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task15.task1504;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,8 +14,20 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        File
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        InputStream inputStream = Files.newInputStream(Path.of(scanner.nextLine()));
+        OutputStream outputStream = Files.newOutputStream(Path.of(scanner.nextLine()));
+        byte[] buffer = inputStream.readAllBytes();
+        for (int i = 0; i < buffer.length - 2; i += 2) {
+            byte temp;
+            temp = buffer[i];
+            buffer[i] = buffer[i + 1];
+            buffer[i + 1] = temp;
+        }
+        outputStream.write(buffer);
+        inputStream.close();
+        outputStream.close();
     }
 }
 
