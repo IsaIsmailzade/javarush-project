787bd39edd107d420544b59a713f9bb7573e6f07
Isa <isaismzade@gmail.com>
2024-05-31 20:29:19 +0400

    .

--- src/com/javarush/task/pro/task15/task1511/Solution.java
+++ src/com/javarush/task/pro/task15/task1511/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task15.task1511;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,11 +17,12 @@ public class Solution {
         char[] chars = args[0].toCharArray();
         try (InputStream stream = System.in;
              Scanner scanner = new Scanner(stream)) {
-            Files.write(Path.of(scanner.nextLine()), chars);
+            Path path = Path.of(scanner.nextLine());
+            BufferedWriter bufferedWriter = Files.newBufferedWriter(path);
+            bufferedWriter.write(chars);
+            bufferedWriter.close();
         } catch (IOException e) {
             System.out.println("Something went wrong : " + e);
         }
     }
 }
-
-
