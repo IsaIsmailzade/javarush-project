5b1d4aff652877ce90bc7b74cbee0b61e5ce8673
Isa <isaismzade@gmail.com>
2024-06-01 18:07:23 +0400

    .

---
+++ src/com/javarush/task/pro/task15/task1516/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        do {
+            s = scanner.nextLine();
+            Path path = Path.of(s);
+            if (Files.isRegularFile(path)) {
+                System.out.println(s + THIS_IS_FILE);
+            } else if (Files.isDirectory(path)) {
+                System.out.println(s + THIS_IS_DIR);
+            } else break;
+        } while (true);
+    }
+}
