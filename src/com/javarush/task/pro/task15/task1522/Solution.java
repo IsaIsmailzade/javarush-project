2b0a43a7879428d67c9e4352d4d606dca15172d7
Isa <isaismzade@gmail.com>
2024-06-02 18:17:01 +0400

    .

--- src/com/javarush/task/pro/task15/task1522/Solution.java
+++ src/com/javarush/task/pro/task15/task1522/Solution.java
@@ -11,6 +11,6 @@ import java.net.URL;
 public class Solution {
     public static void main(String[] args) throws IOException {
         URL url = new URL("https://javarush.ru/api/1.0/rest/projects");
-        //напишите тут ваш код
+
     }
 }
