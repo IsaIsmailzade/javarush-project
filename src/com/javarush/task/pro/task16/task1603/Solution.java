37386edf293a6b603c6944540c0c4f483cf87c54
Isa <isaismzade@gmail.com>
2024-06-04 21:08:33 +0400

    .

--- src/com/javarush/task/pro/task16/task1603/Solution.java
+++ src/com/javarush/task/pro/task16/task1603/Solution.java
@@ -11,8 +11,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Date date = new Date("6 January 2010");
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
-        formatter.format(date.getMonth() + 1);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         System.out.println(formatter.format(date));
     }
 }
