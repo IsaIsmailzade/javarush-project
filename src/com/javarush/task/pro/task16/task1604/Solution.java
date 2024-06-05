83d5e19a646201da733fc88c8bf2b17c2ef44395
Isa <isaismzade@gmail.com>
2024-06-05 14:52:32 +0400

    .

---
+++ src/com/javarush/task/pro/task16/task1604/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate = new GregorianCalendar(2006, 0, 4);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Calendar calendar) {
+        String day = null;
+        switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+            case 1:
+                day = "Воскресенье";
+                break;
+            case 2:
+                day = "Понедельник";
+                break;
+            case 3:
+                day = "Вторник";
+                break;
+            case 4:
+                day = "Среда";
+                break;
+            case 5:
+                day = "Четверг";
+                break;
+            case 6:
+                day = "Пятница";
+                break;
+            case 7:
+                day = "Суббота";
+                break;
+        }
+        return day;
+    }
+}
